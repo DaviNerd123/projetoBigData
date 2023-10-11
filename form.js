3984539e5f3013ccc3d68f14197c8c8ce56124c5
Davi <davideleteribeiro@gmail.com>
2023-10-10 21:53:33 -0300

    s

--- form.js
+++ form.js
@@ -1,13 +1,41 @@
 const body = document.getElementById("body");
 
 function inicializar() {
+  var Tipos = [
+    (Comida = []),
+    (Bebida = []),
+    (Talher = []),
+    (Louca = []),
+    (Decoracao = []),
+  ];
   const productList = document.getElementById("product-list");
   const totalPrice = document.getElementById("total-price");
   const productForm = document.getElementById("product-form");
   const tabela = [];
   const tabelaE = document.getElementById("corpoTabela");
+  var PrecoC = 0;
+  var PrecoB = 0;
+  var PrecoT = 0;
+  var PrecoL = 0;
+  var PrecoD = 0;
 
   let products = [];
+  const chartCanvas = document.getElementById("myChart");
+  const data = {
+    labels: ["Comida", "Bebida", "Talher", "Louça", "Decoração"],
+    datasets: [
+      {
+        data: [0, 0, 0, 0, 0],
+        backgroundColor: ["blue", "pink", "red", "green", "purple"],
+      },
+    ],
+  };
+
+  const ctx = chartCanvas.getContext("2d");
+  const myChart = new Chart(ctx, {
+    type: "pie",
+    data: data,
+  });
 
   productForm.addEventListener("submit", function (event) {
     event.preventDefault();
@@ -18,51 +46,32 @@ function inicializar() {
       (acc, product) => acc + product.total,
       0
     );
-    totalPrice.textContent = `Total a Pagar: R$ ${totalPriceValue.toFixed(2)}`;
-  }
-
-  function createListItem(product) {
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `<p style"margin-right:10px;">${product.name}</p>`;
-    listItem.style.marginRight = "10px";
-    listItem.style.display = "flex";
-    listItem.style.justifyContent = "center";
-    listItem.style.alignContent = "center";
-
-    const editButton = document.createElement("button");
-    editButton.style.height = "20px";
-    editButton;
-    editButton.textContent = "Editar";
-    editButton.addEventListener("click", function () {
-      document.getElementById("product-name").value = product.name;
-      document.getElementById("product-price").value = product.price;
-      document.getElementById("product-quantity").value = product.quantity;
-
-      // Remove o produto da lista ao editar
-
-      const index = products.indexOf(product);
-      tabela.splice(index, 1);
-      tabelaE.innerHTML = "";
-      tabela.forEach(function (produto) {
-        alert(produto.name);
-        tabelaE.innerHTML =
-          tabelaE.innerHTML +
-          `<tr><td>${produto.name}</td><td>${produto.quantity}</td><td>${produto.price}</td><td>${produto.total}</td></tr>`;
-      });
-      if (index !== -1) {
-        products.splice(index, 1);
-        productList.removeChild(listItem);
-
-        updateTotalPrice();
-      }
+    PrecoC = 0;
+    PrecoB = 0;
+    PrecoT = 0;
+    PrecoL = 0;
+    PrecoD = 0;
+    Tipos[0].forEach((x) => {
+      PrecoC = PrecoC + x.price;
     });
-
-    listItem.appendChild(editButton);
-    productList.appendChild(listItem);
+    Tipos[1].forEach((x) => {
+      PrecoB = PrecoB + x.price;
+    });
+    Tipos[2].forEach((x) => {
+      PrecoT = PrecoT + x.price;
+    });
+    Tipos[3].forEach((x) => {
+      PrecoL = PrecoL + x.price;
+    });
+    Tipos[4].forEach((x) => {
+      PrecoD = PrecoD + x.price;
+    });
+    totalPrice.textContent = `Total a Pagar: R$ ${totalPriceValue.toFixed(2)}`;
   }
 
   document.getElementById("add-product").addEventListener("click", function () {
     const productName = document.getElementById("product-name").value;
+    const productType = document.getElementById("Tipo").value;
     const productPrice = parseFloat(
       document.getElementById("product-price").value
     );
@@ -79,14 +88,44 @@ function inicializar() {
       const productTotal = productPrice * productQuantity;
       const product = {
         name: productName,
+        tipo: productType,
         price: productPrice,
         quantity: productQuantity,
         total: productTotal,
       };
+      if (product.tipo == "Comida") {
+        Tipos[0].push(product);
+      } else {
+        if (product.tipo == "Bebida") {
+          Tipos[1].push(product);
+          alert(Tipos[1]);
+        } else {
+          if (product.tipo == "Talher") {
+            Tipos[2].push(product);
+          } else {
+            if (product.tipo == "Louca") {
+              Tipos[3].push(product);
+            } else {
+              Tipos[4].push(product);
+            }
+          }
+        }
+      }
+      if (product.tipo === "Comida") {
+        data.datasets[0].data[0] = product.total + data.datasets[0].data[0];
+      } else if (product.tipo === "Bebida") {
+        data.datasets[0].data[1] = product.total + data.datasets[0].data[1];
+      } else if (product.tipo === "Talher") {
+        data.datasets[0].data[2] = product.total + data.datasets[0].data[2];
+      } else if (product.tipo === "Louça") {
+        data.datasets[0].data[3] = product.total + data.datasets[0].data[3];
+      } else if (product.tipo === "Decoração") {
+        data.datasets[0].data[4] = product.total + data.datasets[0].data[4];
+      }
 
+      myChart.update();
       products.push(product);
       tabela.push(product);
-      createListItem(product);
       updateTotalPrice();
 
       var ultimo = tabela.length - 1;
@@ -99,275 +138,10 @@ function inicializar() {
       tabela.forEach(function (produto) {
         tabelaE.innerHTML =
           tabelaE.innerHTML +
-          `<tr><td>${produto.name}</td><td>${produto.quantity}</td><td>${produto.price}</td><td>${produto.total}</td></tr>`;
+          `<tr><td>${produto.name}</td><td>${produto.tipo}</td><td>${produto.quantity}</td><td>${produto.price}</td><td>${produto.total}</td></tr>`;
       });
     }
   });
 }
-const Finalizar = function () {
-  body = ``
-};
-
-const debutante = function () {
-  var ListadeConvidados = convidados;
-  body.innerHTML = `<body>
-<menu>
-    <div id='bmenu'>
-        <div>
-            <a href='index.html'>Lobby</a>
-            
-            <a href='convites.html'>Convites</a>
-        </div>
-        <div id='login'>
-            <div>
-                <a href='''></a>
-            </div>
-        </div>
-    </div>
-</menu>
-<header>
-    <div>
-         <h1>Festa: Debutante</h1>
-    </div>
-</header>
-<h1>Calculadora de Preço Total</h1>
-<p>Adicione todos os produtos a serem usados na festa (pratos, copos, guardanapos e etc)</p>
-<form id='product-form'>
-    <div id='label'><label for='product-name' id='name'>Nome do Produto:</label>
-    <input type='text' id='product-name' name='product-name' required></div>
-    <div id='label'><label for='product-price'>Preço por Unidade:</label>
-    <input type='number' id='product-price' name='product-price' required></div>
-    <div id='label'><label for='product-quantity' id='quantidade'>Quantidade:</label>
-    <input type='number' id='product-quantity' name='product-quantity' required></div>
-    
-    <button type='button' id='add-product'>Adicionar Produto</button>
-</form>
-
-<h2>Lista de Produtos</h2>
-<ul id='product-list'></ul>
-
-<h2>Total a Pagar:</h2>
-<p id='total-price'>R$ 0.00</p>
-<div id="t">
-    <table border='1'>
-            <thead><tr><th>Produto</th><th>Quantidade</th><th>preço</th><th>Total</th></tr></thead>
-            <tbody id='corpoTabela'>
-            </tbody>
-        </table>
-        </div>
-        <button onclick='Finalizar'>Finalizar</button>
-<script src='../form.js'></script>
-</body>`;
-  inicializar();
-};
-const casamento = function () {
-  var ListadeConvidados = convidados;
-  body.innerHTML = `<body>
-<menu>
-    <div id='bmenu'>
-        <div>
-            <a href='index.html'>Lobby</a>
-            
-            <a href='convites.html'>Convites</a>
-        </div>
-        <div id='login'>
-            <div>
-                <a href='''></a>
-            </div>
-        </div>
-    </div>
-</menu>
-<header>
-    <div>
-         <h1>Festa: Casamento</h1>
-    </div>
-</header>
-<h1>Calculadora de Preço Total</h1>
-<p>Adicione todos os produtos a serem usados na festa (pratos, copos, guardanapos e etc)</p>
-<form id='product-form'>
-    <div id='label'><label for='product-name' id='name'>Nome do Produto:</label>
-    <input type='text' id='product-name' name='product-name' required></div>
-    <div id='label'><label for='product-price'>Preço por Unidade:</label>
-    <input type='number' id='product-price' name='product-price' required></div>
-    <div id='label'><label for='product-quantity' id='quantidade'>Quantidade:</label>
-    <input type='number' id='product-quantity' name='product-quantity' required></div>
-    
-    <button type='button' id='add-product'>Adicionar Produto</button>
-</form>
-
-<h2>Lista de Produtos</h2>
-<ul id='product-list'></ul>
-
-<h2>Total a Pagar:</h2>
-<p id='total-price'>R$ 0.00</p>
-<div id="t">
-    <table border='1'>
-            <thead><tr><th>Produto</th><th>Quantidade</th><th>preço</th><th>Total</th></tr></thead>
-            <tbody id='corpoTabela'>
-            </tbody>
-        </table>
-        </div>
-        <button onclick='Finalizar'>Finalizar</button>
-<script src='../form.js'></script>
-</body>`;
-  inicializar();
-};
-const empresarial = function () {
-  var ListadeConvidados = convidados;
-  body.innerHTML = `<body>
-<menu>
-    <div id='bmenu'>
-        <div>
-            <a href='index.html'>Lobby</a>
-            
-            <a href='convites.html'>Convites</a>
-        </div>
-        <div id='login'>
-            <div>
-                <a href='''></a>
-            </div>
-        </div>
-    </div>
-</menu>
-<header>
-    <div>
-         <h1>Festa: Empresarial</h1>
-    </div>
-</header>
-<h1>Calculadora de Preço Total</h1>
-<p>Adicione todos os produtos a serem usados na festa (pratos, copos, guardanapos e etc)</p>
-<form id='product-form'>
-    <div id='label'><label for='product-name' id='name'>Nome do Produto:</label>
-    <input type='text' id='product-name' name='product-name' required></div>
-    <div id='label'><label for='product-price'>Preço por Unidade:</label>
-    <input type='number' id='product-price' name='product-price' required></div>
-    <div id='label'><label for='product-quantity' id='quantidade'>Quantidade:</label>
-    <input type='number' id='product-quantity' name='product-quantity' required></div>
-    
-    <button type='button' id='add-product'>Adicionar Produto</button>
-</form>
-
-<h2>Lista de Produtos</h2>
-<ul id='product-list'></ul>
-
-<h2>Total a Pagar:</h2>
-<p id='total-price'>R$ 0.00</p>
-<div id="t">
-    <table border='1'>
-            <thead><tr><th>Produto</th><th>Quantidade</th><th>preço</th><th>Total</th></tr></thead>
-            <tbody id='corpoTabela'>
-            </tbody>
-        </table>
-        </div>
-        <button onclick='Finalizar'>Finalizar</button>
-<script src='../form.js'></script>
-</body>`;
-  inicializar();
-};
-const infantil = function () {
-  var ListadeConvidados = convidados;
-  body.innerHTML = `<body>
-<menu>
-    <div id='bmenu'>
-        <div>
-            <a href='index.html'>Lobby</a>
-            
-            <a href='convites.html'>Convites</a>
-        </div>
-        <div id='login'>
-            <div>
-                <a href='''></a>
-            </div>
-        </div>
-    </div>
-</menu>
-<header>
-    <div>
-         <h1>Festa: Infantil</h1>
-    </div>
-</header>
-<h1>Calculadora de Preço Total</h1>
-<p>Adicione todos os produtos a serem usados na festa (pratos, copos, guardanapos e etc)</p>
-<form id='product-form'>
-    <div id='label'><label for='product-name' id='name'>Nome do Produto:</label>
-    <input type='text' id='product-name' name='product-name' required></div>
-    <div id='label'><label for='product-price'>Preço por Unidade:</label>
-    <input type='number' id='product-price' name='product-price' required></div>
-    <div id='label'><label for='product-quantity' id='quantidade'>Quantidade:</label>
-    <input type='number' id='product-quantity' name='product-quantity' required></div>
-    
-    <button type='button' id='add-product'>Adicionar Produto</button>
-</form>
-
-<h2>Lista de Produtos</h2>
-<ul id='product-list'></ul>
-
-<h2>Total a Pagar:</h2>
-<p id='total-price'>R$ 0.00</p>
-<div id="t">
-    <table border='1'>
-            <thead><tr><th>Produto</th><th>Quantidade</th><th>preço</th><th>Total</th></tr></thead>
-            <tbody id='corpoTabela'>
-            </tbody>
-        </table>
-        </div>
-        <button onclick='Finalizar'>Finalizar</button>
-<script src='../form.js'></script>
-</body>`;
-  inicializar();
-};
-const beneficente = function () {
-  var ListadeConvidados = convidados;
-
-  body.innerHTML = `<body>
-<menu>
-    <div id='bmenu'>
-        <div>
-            <a href='index.html'>Lobby</a>
-            
-            <a href='convites.html'>Convites</a>
-        </div>
-        <div id='login'>
-            <div>
-                <a href='''></a>
-            </div>
-        </div>
-    </div>
-</menu>
-<header>
-    <div>
-         <h1>Festa: Beneficente</h1>
-    </div>
-</header>
-<h1>Calculadora de Preço Total</h1>
-<p>Adicione todos os produtos a serem usados na festa (pratos, copos, guardanapos e etc)</p>
-<form id='product-form'>
-    <div id='label'><label for='product-name' id='name'>Nome do Produto:</label>
-    <input type='text' id='product-name' name='product-name' required></div>
-    <div id='label'><label for='product-price'>Preço por Unidade:</label>
-    <input type='number' id='product-price' name='product-price' required></div>
-    <div id='label'><label for='product-quantity' id='quantidade'>Quantidade:</label>
-    <input type='number' id='product-quantity' name='product-quantity' required></div>
-    
-    <button type='button' id='add-product'>Adicionar Produto</button>
-</form>
-
-<h2>Lista de Produtos</h2>
-<ul id='product-list'></ul>
-
-<h2>Total a Pagar:</h2>
-<p id='total-price'>R$ 0.00</p>
-<div id="t">
-    <table border='1'>
-            <thead><tr><th>Produto</th><th>Quantidade</th><th>preço</th><th>Total</th></tr></thead>
-            <tbody id='corpoTabela'>
-            </tbody>
-        </table>
-        </div>
-        <button onclick='Finalizar'>Finalizar</button>
-<script src='../form.js'></script>
-</body>`;
-  inicializar();
-};
 
 inicializar();
